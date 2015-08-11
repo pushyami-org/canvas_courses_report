1efbda0ed6131ccffc18fb326f6a512de0bd5bee
pushyami <pushyami@umich.edu>
2015-08-11 08:22:22 -0400

    TLUNIZIN-667 js formatting stuff

--- src/main/webapp/assets/js/analytics.js
+++ src/main/webapp/assets/js/analytics.js
@@ -1,90 +1,94 @@
   'use strict';
   /* jshint  strict: true*/
-  /* global $, moment, _*/
+  /* global $, Blob, document, window*/
 
-  $(document).ready(function(){
-          getTermsInfo();
 
-          function getTermsInfo(){
-              var url='report?action=getEnrollmentTerms';
-              var termRequest = $.ajax({
-                url: url,
-                   type: 'GET',
-                   dataType: 'json'
-           });
-              termRequest.done(function(data) {
-                  var terms=data.enrollment_terms;
-                  var newSelect=document.getElementById('termsSelect');
-                  var label=document.createElement('label');
-                  label.setAttribute('for', 'termsSelect');
-                   var selectHTML='';
-                   $.each(terms, function(index, term) {
-                           selectHTML+= '<option id="term'+term.id+'" value="'+term.id+'">'+term.name+'</option>';
-                   });
-                   newSelect.innerHTML= selectHTML;
-                   newSelect.appendChild(label);
-                   $('#termsSelect').on('change',function() {
-                      var courseReportRequest = $.ajax({
-                          url: 'report?action=getCoursesPublished&term='+$(this).val()+'&termName='+$('#termsSelect option:selected').text(),
-                          type: 'GET',
+  $(document).ready(function() {
+      getTermsInfo();
+
+      function getTermsInfo() {
+          var url = 'report?action=getEnrollmentTerms';
+          var termRequest = $.ajax({
+              url: url,
+              type: 'GET',
+              dataType: 'json'
+          });
+          termRequest.done(function(data) {
+              var terms = data.enrollment_terms;
+              var newSelect = document.getElementById('termsSelect');
+              var label = document.createElement('label');
+              label.setAttribute('for', 'termsSelect');
+              var selectHTML = '';
+              $.each(terms, function(index, term) {
+                  selectHTML += '<option id="term' + term.id + '" value="' + term.id + '">' + term.name + '</option>';
+              });
+              newSelect.innerHTML = selectHTML;
+              newSelect.appendChild(label);
+              $('#termsSelect').on('change', function() {
+                  var courseReportRequest = $.ajax({
+                      url: 'report?action=getCoursesPublished&term=' + $(this).val() + '&termName=' + $('#termsSelect option:selected').text(),
+                      type: 'GET',
                   });
-                      courseReportRequest.done(function(response, status, xhr){
-                           var filename = '';
-                           var disposition = xhr.getResponseHeader('Content-Disposition');
-                           if (disposition && disposition.indexOf('attachment') !== -1) {
-                               var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
-                               var matches = filenameRegex.exec(disposition);
-                               if (matches !== null && matches[1]) filename = matches[1].replace(/['"]/g, '');
-                       }
+                  courseReportRequest.done(function(response, status, xhr) {
+                      var filename = '';
+                      var disposition = xhr.getResponseHeader('Content-Disposition');
+                      if (disposition && disposition.indexOf('attachment') !== -1) {
+                          var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                          var matches = filenameRegex.exec(disposition);
+                          if (matches !== null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+                      }
 
-                       var type = xhr.getResponseHeader('Content-Type');
-                       var blob = new Blob([response], { type: type });
+                      var type = xhr.getResponseHeader('Content-Type');
+                      var blob = new Blob([response], {
+                          type: type
+                      });
 
-                       if (typeof window.navigator.msSaveBlob !== 'undefined') {
-                                              // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
-                                              window.navigator.msSaveBlob(blob, filename);
-                                      } else {
-                                               var URL = window.URL || window.webkitURL;
-                                               var downloadUrl = URL.createObjectURL(blob);
+                      if (typeof window.navigator.msSaveBlob !== 'undefined') {
+                          // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
+                          window.navigator.msSaveBlob(blob, filename);
+                      } else {
+                          var URL = window.URL || window.webkitURL;
+                          var downloadUrl = URL.createObjectURL(blob);
 
-                                               if (filename) {
-                                                      // use HTML5 a[download] attribute to specify filename
-                                                      var a = document.createElement('a');
-                                                      // safari doesn't support this yet
-                                                      if (typeof a.download === 'undefined') {
-                                                               window.location = downloadUrl;
-                                                       } else {
-                                                               a.href = downloadUrl;
-                                                               a.download = filename;
-                                                               document.body.appendChild(a);
-                                                               a.click();
-                                                       }
-                                               } else {
-                                                       window.location = downloadUrl;
-                                               }
+                          if (filename) {
+                              // use HTML5 a[download] attribute to specify filename
+                              var a = document.createElement('a');
+                              // safari doesn't support this yet
+                              if (typeof a.download === 'undefined') {
+                                  window.location = downloadUrl;
+                              } else {
+                                  a.href = downloadUrl;
+                                  a.download = filename;
+                                  document.body.appendChild(a);
+                                  a.click();
+                              }
+                          } else {
+                              window.location = downloadUrl;
+                          }
 
-                                              setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
-                                      }
-                              });
+                          setTimeout(function() {
+                              URL.revokeObjectURL(downloadUrl);
+                          }, 100); // cleanup
+                      }
+                  });
 
-  courseReportRequest.fail(function(xhr) {
-          if(xhr.status===403){
+                  courseReportRequest.fail(function(xhr) {
+                      if (xhr.status === 403) {
+                          $('#sessExpire').show();
+                      }
+                      alert(xhr.responseText);
+                  });
+
+              });
+
+          });
+          termRequest.fail(function(xhr) {
+              if (xhr.status === 403) {
                   $('#sessExpire').show();
-          }
-          alert(xhr.responseText);
-  });
+              }
+              alert(xhr.responseText);
+          });
+
+      }
 
   });
-
-  });
-  termRequest.fail(function(xhr, textStatus) {
-      if(xhr.status===403){
-           $('#sessExpire').show();
-   }
-   alert(xhr.responseText);
-  });
-
-  }
-
-  });
-
